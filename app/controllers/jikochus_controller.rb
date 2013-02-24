fde53fab8681253443e7ef169c088d19d1a78569
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2013-02-24 21:54:40 +0900

    add (selfish) request action

--- app/controllers/jikochus_controller.rb
+++ app/controllers/jikochus_controller.rb
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+# GistID: 5023699
+
+# 自己中な (Selfish) Controller
 class JikochusController < ApplicationController
-  # GET /jikochus
-  # GET /jikochus.json
+
   def index
     @jikochus = Jikochu.all
 
@@ -8,76 +11,15 @@ class JikochusController < ApplicationController
       format.html # index.html.erb
       format.json { render json: @jikochus }
     end
+    #=> SystemStackError in JikochusController#index
+    #=> stack level too deep
   end
 
-  # GET /jikochus/1
-  # GET /jikochus/1.json
-  def show
-    @jikochu = Jikochu.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @jikochu }
-    end
-  end
-
-  # GET /jikochus/new
-  # GET /jikochus/new.json
-  def new
-    @jikochu = Jikochu.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @jikochu }
-    end
-  end
-
-  # GET /jikochus/1/edit
-  def edit
-    @jikochu = Jikochu.find(params[:id])
-  end
-
-  # POST /jikochus
-  # POST /jikochus.json
-  def create
-    @jikochu = Jikochu.new(params[:jikochu])
-
-    respond_to do |format|
-      if @jikochu.save
-        format.html { redirect_to @jikochu, notice: 'Jikochu was successfully created.' }
-        format.json { render json: @jikochu, status: :created, location: @jikochu }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @jikochu.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /jikochus/1
-  # PUT /jikochus/1.json
-  def update
-    @jikochu = Jikochu.find(params[:id])
-
-    respond_to do |format|
-      if @jikochu.update_attributes(params[:jikochu])
-        format.html { redirect_to @jikochu, notice: 'Jikochu was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @jikochu.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /jikochus/1
-  # DELETE /jikochus/1.json
-  def destroy
-    @jikochu = Jikochu.find(params[:id])
-    @jikochu.destroy
-
-    respond_to do |format|
-      format.html { redirect_to jikochus_url }
-      format.json { head :no_content }
-    end
+  def request # 自己中な (selfish) request
+    # 自己中なことにrequestを上書きしてしまう
+    # 結果、他のアクションでも SystemStackError が起こる
+    # It's as selfish as overriding the request method,
+    # which causes SystemStackError in its other actions.
+    render text: "どけどけどけー！オレのアクションだー！"
   end
 end
